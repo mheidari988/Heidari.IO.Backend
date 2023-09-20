cf331e455a17455a2ee32e1eb56ae657d5b64dce
Reza Heidari <m.heidari988@gmail.com>
2023-09-20 18:06:50 +0300

    use base entity as parent
    also converted classes to the records for consistency and safety.

--- src/Application/Portfolios/Queries/GetPortfolio/Dtos/MenuDto.cs
+++ src/Application/Portfolios/Queries/GetPortfolio/Dtos/MenuDto.cs
@@ -1,5 +1,8 @@
 namespace backend.Application.Portfolios.Queries.GetPortfolio.Dtos;
-public class MenuDto
+
+using backend.Application.Common.Entities;
+
+public record MenuDto : Entity
 {
     public string Title { get; set; }
     public string Url { get; set; }
