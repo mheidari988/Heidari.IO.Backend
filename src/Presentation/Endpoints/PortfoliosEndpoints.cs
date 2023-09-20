f137e8dd2613029a17993f093d1a6e627aa0d323
Reza Heidari <m.heidari988@gmail.com>
2023-09-20 16:54:03 +0300

    Create PortfoliosEndpoints.cs

---
+++ src/Presentation/Endpoints/PortfoliosEndpoints.cs
@@ -0,0 +1,34 @@
+namespace backend.Presentation.Endpoints;
+
+using backend.Application.Portfolios.Queries.GetPortfolio;
+using MediatR;
+
+public static class PortfoliosEndpoints
+{
+    public static WebApplication MapPortfoliosEndpoints(this WebApplication app)
+    {
+        var root = app.MapGroup("portfolio")
+            .WithTags("portfolio")
+            .WithOpenApi();
+
+        _ = root.MapGet("/", GetPortfolio)
+            .Produces<GetPortfolioResponse>()
+            .ProducesProblem(StatusCodes.Status500InternalServerError)
+            .WithSummary("Lookup the portfolio")
+            .WithDescription("\n    GET /portfolio");
+
+        return app;
+    }
+
+    public static async Task<IResult> GetPortfolio(IMediator mediator)
+    {
+        try
+        {
+            return Results.Ok(await mediator.Send(new GetPortfolioQuery()));
+        }
+        catch (Exception ex)
+        {
+            return Results.Problem(ex.StackTrace, ex.Message, StatusCodes.Status500InternalServerError);
+        }
+    }
+}
