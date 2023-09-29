03f94ffc7f6aca92ac5f5816d24127bacd6010cb
Reza Heidari <m.heidari988@gmail.com>
2023-09-29 18:16:40 +0300

    update infrastructure with proper dependencies

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -8,12 +8,13 @@ using backend.Infrastructure.Databases.WebContents;
 using backend.Infrastructure.Databases.WebContents.Repositories;
 using Databases.MoviesReviews;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SimpleDateTimeProvider;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration, string environmentName)
     {
         _ = services.AddDbContext<MovieReviewsDbContext>(options => options.UseInMemoryDatabase($"Movies-{Guid.NewGuid()}"), ServiceLifetime.Singleton);
 
@@ -29,10 +30,27 @@ public static class DependencyInjection
         _ = services.AddSingleton<IDateTimeProvider, SystemDateTimeProvider>();
 
 
-        // WebContents
+        //// WebContents
 
-        _ = services.AddDbContext<WebContentsDbContext>(options => options.UseInMemoryDatabase($"WebContents-{Guid.NewGuid()}"), ServiceLifetime.Singleton);
-        _ = services.AddSingleton<IPortfoliosRepository, PortfoliosRepository>();
+        var cosmosDbSettings = configuration.GetSection("Cosmos");
+        var accountEndpoint = cosmosDbSettings["AccountEndpoint"];
+        var accountKey = cosmosDbSettings["AccountKey"];
+        var databaseName = cosmosDbSettings["DatabaseName"];
+
+        // If running locally, use the local settings from appsettings.json
+        if (environmentName != "local")
+        {
+            accountEndpoint = configuration["COSMOS_ENDPOINT"];
+            accountKey = configuration["COSMOS_KEY"];
+            databaseName = configuration["COSMOS_DBNAME"];
+        }
+
+        _ = services.AddDbContext<WebContentsDbContext>(options => options.UseCosmos(
+                accountEndpoint: accountEndpoint,
+                accountKey: accountKey,
+                databaseName: databaseName));
+
+        _ = services.AddScoped<IPortfoliosRepository, PortfoliosRepository>();
 
         return services;
     }
