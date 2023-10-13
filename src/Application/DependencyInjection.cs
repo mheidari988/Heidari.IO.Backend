02aab82567903e3cf4c0bef0d12ba48d3a760907
Reza Heidari <m.heidari988@gmail.com>
2023-10-13 18:32:21 +0300

    add recaptcha and update mediatr to v12

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,10 +1,9 @@
 namespace backend.Application;
 
 using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-using MediatR;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using reCAPTCHA.AspNetCore;
 
 [ExcludeFromCodeCoverage]
 public static class DependencyInjection
@@ -14,7 +13,10 @@ public static class DependencyInjection
         // TODO: Get the base address from configuration
         _ = services.AddHttpClient("azure-blob-storage", c => c.BaseAddress = new Uri("https://heidaristorage.blob.core.windows.net"));
 
-        _ = services.AddMediatR(Assembly.GetExecutingAssembly());
+        _ = services.AddMediatR(config => config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+        _ = services.Configure<RecaptchaSettings>(configuration.GetSection("RecaptchaSettings"));
+        _ = services.AddTransient<IRecaptchaService, RecaptchaService>();
 
         return services;
     }
