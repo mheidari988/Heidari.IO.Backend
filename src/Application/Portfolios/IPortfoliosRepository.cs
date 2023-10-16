cd9fea608960e071a7ff5ca551c4dea695b24c95
Reza Heidari <m.heidari988@gmail.com>
2023-10-16 16:38:12 +0300

    rename to follow naming conventions

--- src/Application/Portfolios/IPortfoliosRepository.cs
+++ src/Application/Portfolios/IPortfoliosRepository.cs
@@ -7,7 +7,7 @@ using backend.Application.Portfolios.Queries.GetPortfolio;
 
 public interface IPortfoliosRepository
 {
-    Task<List<GetExperiencesResponse>> GetExperiences(GetExperiencesQuery requestQuery, CancellationToken cancellationToken = default);
+    Task<List<GetExperiencesResponse>> GetExperiencesAsync(GetExperiencesQuery requestQuery, CancellationToken cancellationToken = default);
     Task<GetPortfolioResponse> GetPortfolioAsync(CancellationToken cancellationToken = default);
     Task<ContactMeResponse> SaveContactMe(ContactMeCommand request, CancellationToken cancellationToken = default);
 }
